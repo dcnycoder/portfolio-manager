a9ea9d35c8ef47c49cd8c5d6a2f8c7e9829ad812
Dennis C <dc5stars@gmail.com>
2019-10-24 20:19:40 -0400

    1st PC commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize')
 
 const db = new Sequelize('postgres://localhost:5432/portfolioManager', {
+  // host: 'localhost',
+  // dialect: 'postgres',
   logging: false
 })
 
